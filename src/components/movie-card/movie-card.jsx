1526a733c3ca00aad01a9c4bd7c14f054c8ade9f
serinhanw <79671093+serinhanw@users.noreply.github.com>
2021-10-01 17:44:42 -0700

    added proptypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
+    /* two props: one func onMovieClick, and one object movieData */
     const { movieData, onMovieClick } = this.props;
     return (
       <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>
-        <div className="movie-poster">
+        {/* <div className="movie-poster">
           <img src={movieData.ImagePath} />
-        </div>
+        </div> */}
         <div className="movie-title">
           <span>{movieData.Title}</span>
+        </div>
+        <div className="movie-year">
+          <span>{movieData.Year}</span>
         </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movieData: PropTypes.shape({
+    ImagePath: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Year: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,23 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieView extends React.Component {
+
+  // keypressCallback(event) {
+  //   console.log(event.key);
+  // }
+
+  // componentDidMount() {
+  //   document.addEventListener('keypress', this.keypressCallback);
+  // }
+
+  // componentWillUnmount() {
+  //   document.removeEventListener('keypress', this.keypressCallback);
+  // }
+
   render() {
     const { movieData, onBackClick } = this.props;
     return (
       <div className="movie-view">
-        <div className="movie-poster">
+        {/* <div className="movie-poster">
           <img src={movieData.ImagePath} />
-        </div>
+        </div> */}
         <div className="movie-title">
           <span className="label">Title: </span>
           <span className="value">{movieData.Title}</span>
         </div>
+        <div className="movie-year">
+          <span className="label">Year: </span>
+          <span className="value">{movieData.Year}</span>
+        </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movieData.Genre}</span>
+        </div>
         <div className="movie-description">
           <span className="label">Description: </span>
           <span className="value">{movieData.Description}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movieData.Director}</span>
         </div>
         <button onClick={() => { onBackClick(null); }}>Back</button>
       </div>
     );
   }
 }
+
+MovieView.propTypes = {
+  movieData: PropTypes.shape({
+    ImagePath: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Year: PropTypes.string.isRequired,
+    // Genre: PropTypes.shape({
+    //   Name: PropTypes.string,
+    //   Description: PropTypes.string,
+    // }),
+    Description: PropTypes.string.isRequired,
+    // Director: PropTypes.shape({
+    //   Name: PropTypes.string.isRequired,
+    // }),
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
+};
