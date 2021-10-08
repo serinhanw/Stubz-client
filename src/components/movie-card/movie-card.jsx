06c32317f28538fd779be39cffe7e14e1f501f46
serinhanw <79671093+serinhanw@users.noreply.github.com>
2021-10-08 03:34:14 -0700

    movie card update
    w/ bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
   render() {
     /* two props: one func onMovieClick, and one object movieData */
     const { movieData, onMovieClick } = this.props;
+
     return (
-      <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>
-        {/* <div className="movie-poster">
-          <img src={movieData.ImagePath} />
-        </div> */}
-        <div className="movie-title">
-          <span>{movieData.Title}</span>
-        </div>
-        <div className="movie-year">
-          <span>{movieData.Year}</span>
-        </div>
-      </div>
+      <Card /*className="h-auto"*/>
+        <Card.Img className="movie-card-link" width={300} height={310} src={movieData.ImagePath} alt={movieData.Title + " Poster"} crossOrigin="anonymous" onClick={() => onMovieClick(movieData)} />
+        {/* <Card.Body>
+          <Card.Title>{movieData.Title}</Card.Title>
+          <Card.Text className="truncate">{movieData.Description}</Card.Text>
+          <Button onClick={() => onMovieClick(movieData)} variant="primary">Details</Button>
+        </Card.Body> */}
+      </Card>
     );
   }
 }
@@ -27,5 +29,17 @@ MovieCard.propTypes = {
     Title: PropTypes.string.isRequired,
     Year: PropTypes.string.isRequired
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  onMovieClick: PropTypes.func.isRequired,
 };
+
+// <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>
+      //   {/* <div className="movie-poster">
+      //     <img src={movieData.ImagePath} />
+      //   </div> */}
+      //   <div className="movie-title">
+      //     <span>{movieData.Title}</span>
+      //   </div>
+      //   <div className="movie-year">
+      //     <span>{movieData.Year}</span>
+      //   </div>
+      // </div>
