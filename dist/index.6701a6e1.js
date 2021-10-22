1f7f5075944fe9f46c1ebc5276762981ad970564
serinhanw <79671093+serinhanw@users.noreply.github.com>
2021-10-22 03:47:59 -0700

    misc ignore

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -978,29 +978,7 @@ class MyFlixApplication extends _reactDefault.default.Component {
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 // Tells React to render your app in the root DOM element
-_reactDomDefault.default.render(/*#__PURE__*/ _reactDefault.default.createElement(MyFlixApplication), container); /////////////
- // import React from 'react';
- // import ReactDOM from 'react-dom';
- // import MainView from './components/main-view/main-view';
- // import Container from 'react-bootstrap/Container';
- // import Row from 'react-bootstrap/Row';
- // import Col from 'react-bootstrap/Col';
- // // Import statement to indicate that you need to bundle './index.scss'
- // import './index.scss';
- // // Main component (will eventually use all the others)
- // class MyFlixApplication extends React.Component {
- //   render() {
- //     return (
- //       <Container className="container">
- //         <MainView />
- //       </Container>
- //     );
- //   }
- // }
- // // Finds the root of your app
- // const container = document.getElementsByClassName('app-container')[0];
- // // Tells React to render your app in the root DOM element
- // ReactDOM.render(React.createElement(MyFlixApplication), container);
+_reactDomDefault.default.render(/*#__PURE__*/ _reactDefault.default.createElement(MyFlixApplication), container);
 
   $parcel$ReactRefreshHelpers$b058.postlude(module);
 } finally {
