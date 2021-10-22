821003b38774c46c21b8daba22245edeaa191c56
serinhanw <79671093+serinhanw@users.noreply.github.com>
2021-10-22 03:55:02 -0700

    layout and styling update
    -link to registration form created

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+// import PropTypes from "prop-types";
+
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import './login-view.scss';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+import { Link } from "react-router-dom";
 import WebFont from 'webfontloader';
+
+import './login-view.scss';
 
 WebFont.load({
   google: {
     families:
-      ['Anton:400', 'Noto Sans Display:300,400,400italic,500,500italic,600']
+      ['Anton:400', 'Oswald:200,300,400,500,700', 'Noto Sans Display:300,400,400italic,500,500italic,600']
   }
 });
 
@@ -17,25 +26,78 @@ export function LoginView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    axios.post('https://stubz.herokuapp.com/login', {
+      username: username,
+      password: password
+    })
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(data);
+        // window.open("/", "_self");
+      })
+      .catch(e => {
+        console.log('no such user')
+      });
   };
+  //lg={6} md={8} sm={12} 
 
   return (
-    <Form className="login-form">
-      <h1 className="title font-weight-bold text-center">Stubz</h1>
-      <Form.Group controlId="formUsername" className="mb-3">
-        {/* <Form.Label className="form-labels">Username:</Form.Label> */}
-        <Form.Control className="form-labels-placeholder" type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
+    <Row className="justify-content-center align-items-center mx-auto min-vh-100">
+      <Col lg={6} md={8} sm={12} >
+        <Form>
+          <h1 className="title text-center mb-3 font-weight-bold ">Stubz</h1>
+          <Form.Group className="mb-3" controlId="formUsername">
+            <Form.Control
+              className="form-labels-placeholder"
+              placeholder="Enter username"
+              type="text"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+          </Form.Group>
 
-      <Form.Group controlId="formPassword" className="mb-3">
-        {/* <Form.Label className="form-labels">Password:</Form.Label> */}
-        <Form.Control className="form-labels-placeholder" type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Button type="submit" className="login-form-btn" bsPrefix="login-form-btn" onClick={handleSubmit}>Sign in</Button>
-    </Form>
+          <Form.Group className="mb-3" controlId="formPassword">
+            <Form.Control
+              className="form-labels-placeholder"
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Row className="mx-auto">
+            <Button
+              className="login-form-btn"
+              bsPrefix="login-form-btn"
+              type="submit"
+              onClick={handleSubmit}>
+              Sign in
+            </Button>
+          </Row>
+
+          <p className="party-txt text-center mt-5">Ready to party?</p>
+          <p className="text-center">
+            <Link to='/register'>
+              <Button bsPrefix="register-btn" type="submit">Sign up here!</Button>
+            </Link>
+          </p>
+
+
+
+        </Form>
+      </Col>
+    </Row>
   );
 }
+
+// LoginView.propTypes = {
+//   user: PropTypes.shape({
+//     username: PropTypes.string.isRequired,
+//     password: PropTypes.string.isRequired,
+//   }),
+//   onLoggedIn: PropTypes.func.isRequired
+// };
+
+
+
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,82 +1,181 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
+// import { Link } from 'react-router-dom';
+// import WebFont from 'webfontloader';
+
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+
 import './registration-view.scss';
+
+// WebFont.load({
+//   google: {
+//     families:
+//       ['Anton:400', 'Noto Sans Display:300,400,400italic,500,500italic,600']
+//   }
+// });
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [FirstName, setFirstName] = useState("");
-  const [LastName, setLastName] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
 
-  const handleSubmit = () => {
+  const handleRegister = (e) => {
     e.preventDefault();
-    console.log(username, password, FirstName, LastName, email, birthday);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onRegister(username);
+    axios.post('https://stubz.herokuapp.com/users', {
+      FirstName: firstname,
+      LastName: lastname,
+      username: username,
+      password: password,
+      email: email,
+      Birthday: birthday
+    })
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        window.open('/', '_self');
+      })
+      .catch(e => {
+        console.log('error registering the user')
+      });
   };
 
-  return (
-    <div className="registration-form">
-      <Form>
-        <Form.Group className="mb-3" controlId="formFirstName">
-          <Form.Label>First Name:</Form.Label>
-          <Form.Control type="text" onChange={(e) => setFirstName(e.target.value)}></Form.Control>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formLastName">
-          <Form.Label>Last Name:</Form.Label>
-          <Form.Control type="text" onChange={(e) => setLastName(e.target.value)}></Form.Control>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formUsername">
-          <Form.Label>Username:</Form.Label>
-          <Form.Control type="text" onChange={(e) => { setUsername(e.target.value); }}></Form.Control>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formPassword">
-          <Form.Label>Password:</Form.Label>
-          <Form.Control type="password" onChange={(e) => setPassword(e.target.value)}></Form.Control>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formEmail">
-          <Form.Label>Email:</Form.Label>
-          <Form.Control type="email" onChange={(e) => setEmail(e.target.value)}></Form.Control>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBirthday">
-          <Form.Label>Date of Birth:</Form.Label>
-          <Form.Control type="date" onChange={(e) => setBirthday(e.target.value)}></Form.Control>
-        </Form.Group>
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     axios.post('https://stubz.herokuapp.com/users', {
+  //       FirstName: FirstName,
+  //       LastName: LastName,
+  //       username: username,
+  //       password: password,
+  //       email: email,
+  //       Birthday: Birthday
+  //     })
+  //       .then(response => {
+  //         const data = response.data;
+  //         console.log(data);
+  //         window.open('/', '_self');
+  //       })
+  //       .catch(e => {
+  //         console.log('error registering the user')
+  //       });
+  //   };
 
-        <Button className="mt-20" variant="success" type="submit" onClick={handleSubmit}>Sign Up</Button>
-      </Form>
-    </div>
+  return (
+    <Form className="register justify-content-md-center">
+      <Form.Group className="mb-3" controlId="formFirstname">
+        <Form.Label>First Name:*</Form.Label>
+        <Form.Control type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)}></Form.Control>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formLastname">
+        <Form.Label>Last Name:</Form.Label>
+        <Form.Control type="text" value={lastname} onChange={(e) => setLastname(e.target.value)}></Form.Control>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formUsername">
+        <Form.Label>Username:*</Form.Label>
+        <Form.Control type="text" value={username} onChange={(e) => { setUsername(e.target.value); }}></Form.Control>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formPassword">
+        <Form.Label>Password:*</Form.Label>
+        <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formEmail">
+        <Form.Label>Email:*</Form.Label>
+        <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBirthday">
+        <Form.Label>Date of Birth:</Form.Label>
+        <Form.Control type="date" value={birthday} onChange={(e) => setBirthday(e.target.value)}></Form.Control>
+      </Form.Group>
+
+      <p>
+        <small>Fields marked with * are required.</small>
+      </p>
+
+      <Button className="mt-2" variant="success" type="submit" onClick={handleRegister}>Sign Up</Button>
+    </Form>
+
   );
 }
 
-{/* <form>
-      <label>
-        First Name:
-        <input type="text" value={FirstName} onChange={e => setFirstName(e.target.value)} />
-      </label>
-      <label>
-        Last Name:
-        <input type="text" value={LastName} onChange={e => setLastName(e.target.value)} />
-      </label>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Birthday:
-        <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Sign Up</button>
-    </form> */}
+// RegistrationView.propTypes = {
+//   register: PropTypes.shape({
+//     FirstName: PropTypes.string.isRequired,
+//     LastName: PropTypes.string,
+//     username: PropTypes.string.isRequired,
+//     password: PropTypes.string.isRequired,
+//     email: PropTypes.string.isRequired,
+//     Birthday: PropTypes.string.isRequired
+//   }),
+// };
+
+
+
+
+
+
+
+
+
+////// BEFORE MAJOR UPDATES //////
+
+// import React, { useState } from 'react';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+// import './registration-view.scss';
+
+// export function RegistrationView(props) {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [FirstName, setFirstName] = useState("");
+//   const [LastName, setLastName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [birthday, setBirthday] = useState("");
+
+//   const handleSubmit = () => {
+//     e.preventDefault();
+//     console.log(username, password, FirstName, LastName, email, birthday);
+//     /* Send a request to the server for authentication */
+//     /* then call props.onLoggedIn(username) */
+//     props.onRegister(username);
+//   };
+
+//   return (
+//     <div className="registration-form">
+//       <Form>
+//         <Form.Group className="mb-3" controlId="formFirstName">
+//           <Form.Label>First Name:</Form.Label>
+//           <Form.Control type="text" onChange={(e) => setFirstName(e.target.value)}></Form.Control>
+//         </Form.Group>
+//         <Form.Group className="mb-3" controlId="formLastName">
+//           <Form.Label>Last Name:</Form.Label>
+//           <Form.Control type="text" onChange={(e) => setLastName(e.target.value)}></Form.Control>
+//         </Form.Group>
+//         <Form.Group className="mb-3" controlId="formUsername">
+//           <Form.Label>Username:</Form.Label>
+//           <Form.Control type="text" onChange={(e) => { setUsername(e.target.value); }}></Form.Control>
+//         </Form.Group>
+//         <Form.Group className="mb-3" controlId="formPassword">
+//           <Form.Label>Password:</Form.Label>
+//           <Form.Control type="password" onChange={(e) => setPassword(e.target.value)}></Form.Control>
+//         </Form.Group>
+//         <Form.Group className="mb-3" controlId="formEmail">
+//           <Form.Label>Email:</Form.Label>
+//           <Form.Control type="email" onChange={(e) => setEmail(e.target.value)}></Form.Control>
+//         </Form.Group>
+//         <Form.Group className="mb-3" controlId="formBirthday">
+//           <Form.Label>Date of Birth:</Form.Label>
+//           <Form.Control type="date" onChange={(e) => setBirthday(e.target.value)}></Form.Control>
+//         </Form.Group>
+
+//         <Button className="mt-20" variant="success" type="submit" onClick={handleSubmit}>Sign Up</Button>
+//       </Form>
+//     </div>
+//   );
+// }
